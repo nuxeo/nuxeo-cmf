91e9506ba49d6d41765db03722f0b1042d601782
Mariana Cedica <mcedica@nuxeo.com>
2010-10-28 12:38:36 +0200

    NXCM-202 : add comentable steps

--- nuxeo-case-management-web/src/main/java/org/nuxeo/cm/web/comments/CaseManagementCommentActionsBean.java
+++ nuxeo-case-management-web/src/main/java/org/nuxeo/cm/web/comments/CaseManagementCommentActionsBean.java
@@ -29,6 +29,9 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.nuxeo.cm.cases.Case;
+import org.nuxeo.cm.cases.CaseItem;
+import org.nuxeo.cm.web.context.CaseManagementContextHolder;
 import org.nuxeo.cm.web.invalidations.CaseManagementContextBound;
 import org.nuxeo.cm.web.invalidations.CaseManagementContextBoundInstance;
 import org.nuxeo.ecm.core.api.ClientException;
@@ -36,7 +39,7 @@ import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.platform.actions.Action;
 import org.nuxeo.ecm.platform.comment.web.CommentManagerActions;
 import org.nuxeo.ecm.platform.comment.web.ThreadEntry;
-
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 
 /**
  * @author <a href="mailto:ldoguin@nuxeo.com">Laurent Doguin</a>
@@ -47,7 +50,7 @@ import org.nuxeo.ecm.platform.comment.web.ThreadEntry;
 @Install(precedence = Install.FRAMEWORK)
 @CaseManagementContextBound
 public class CaseManagementCommentActionsBean extends
-CaseManagementContextBoundInstance {
+        CaseManagementContextBoundInstance {
 
     private static final long serialVersionUID = 6994714264125928209L;
 
@@ -56,6 +59,17 @@ CaseManagementContextBoundInstance {
     @In(create = true)
     protected transient CommentManagerActions commentManagerActions;
 
+    @In(create = true)
+    protected transient NavigationContext navigationContext;
+
+    @Override
+    public void onMailboxContextChange(
+            CaseManagementContextHolder correspContextHolder)
+            throws ClientException {
+        super.onMailboxContextChange(correspContextHolder);
+        commentManagerActions.documentChanged();
+    }
+
     @Override
     protected void resetCurrentCaseItemCache(DocumentModel cachedEmail,
             DocumentModel newEmail) throws ClientException {
@@ -63,16 +77,38 @@ CaseManagementContextBoundInstance {
     }
 
     public String addComment() throws ClientException {
+        // if in case, add the comment to teh case item
+        if (!isCurrentDocumentCase()) {
+            return commentManagerActions.addComment();
+        }
         return commentManagerActions.createComment(getCurrentCaseItem());
     }
 
     @Factory(value = "caseItemThreadedComments", scope = EVENT)
     public List<ThreadEntry> getCommentsAsThread() throws ClientException {
+        if (!isCurrentDocumentCase()) {
+            return commentManagerActions.getCommentsAsThread(null);
+        }
         return commentManagerActions.getCommentsAsThread(getCurrentCaseItem());
     }
 
-    public List<Action> getActionsForComment() {
+    public List<Action> getActionsForComment() throws ClientException {
+        if (!isCurrentDocumentCase()) {
+            return commentManagerActions.getActionsForComment();
+        }
         return commentManagerActions.getActionsForComment(CASE_MANAGEMENT_COMMENT_ACTIONS);
     }
 
+    protected boolean isCurrentDocumentCase() {
+        DocumentModel currentDoc = navigationContext.getCurrentDocument();
+        Case currentCase = currentDoc.getAdapter(Case.class);
+        if (currentCase != null) {
+            return true;
+        }
+        CaseItem currentCaseItem = currentDoc.getAdapter(CaseItem.class);
+        if (currentCaseItem != null) {
+            return true;
+        }
+        return false;
+    }
 }
