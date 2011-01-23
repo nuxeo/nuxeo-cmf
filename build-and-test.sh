8cbc6e44e9a1f20a71e153113e4cbf105b8461b7
Alexandre Russel <arussel@nuxeo.com>
2011-01-23 18:58:14 +0100

    NXCM-287 - move correspondence into cmf

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 DISTRIBUTION=${1:-"server"}
+DIR_DISTRIB=${2:-"cm"}
 echo "using distribution: $DISTRIBUTION"
 # Build Nuxeo Case Management
 mvn clean install || exit 1
@@ -7,19 +8,19 @@ mvn -P$DISTRIBUTION -f nuxeo-case-management-distribution/pom.xml clean install 
 
 # start JBoss
 (cd nuxeo-case-management-distribution/target && unzip nuxeo-case-management-distribution-*.zip && rm *.zip) || exit 1
-chmod +x nuxeo-case-management-distribution/target/nuxeo-cm-$DISTRIBUTION/bin/nuxeoctl || exit 1
+chmod +x nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl || exit 1
 if [ $DISTRIBUTION = "server" ]
     then
-    sed -i "s/-Xmx1024m/-Xmx2g/" nuxeo-case-management-distribution/target/nuxeo-cm-$DISTRIBUTION/bin/nuxeo.conf
+    
 fi
-nuxeo-case-management-distribution/target/nuxeo-cm-$DISTRIBUTION/bin/nuxeoctl start || exit 1
+nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl start || exit 1
 
 # Run selenium tests
 HIDE_FF=true nuxeo-case-management-distribution/ftest/selenium/run.sh
 ret1=$?
 
 # Strop JBoss
-nuxeo-case-management-distribution/target/nuxeo-cm-$DISTRIBUTION/bin/nuxeoctl stop || exit 1
+nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl stop || exit 1
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
