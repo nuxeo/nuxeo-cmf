7c952459564be074fc556435d1de73544e251632
Alexandre Russel <arussel@nuxeo.com>
2011-01-24 08:47:40 +0100

    NXCM-287 - move correspondence into cmf (fix typo)

--- build-and-test.sh
+++ build-and-test.sh
@@ -10,19 +10,15 @@ mvn -P$PROFILE -f nuxeo-case-management-distribution/pom.xml clean install || ex
 
 # start JBoss
 (cd nuxeo-case-management-distribution/target && unzip nuxeo-case-management-distribution-*.zip && rm *.zip) || exit 1
-chmod +x nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl || exit 1
-if [ $DISTRIBUTION = "server" ]
-    then
-    
-fi
-nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl start || exit 1
+chmod +x nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl || exit 1
+nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl start || exit 1
 
 # Run selenium tests
 HIDE_FF=true nuxeo-case-management-distribution/ftest/selenium/run.sh
 ret1=$?
 
 # Strop JBoss
-nuxeo-case-management-distribution/target/nuxeo-${DIR_STRIB}-$DISTRIBUTION/bin/nuxeoctl stop || exit 1
+nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl stop || exit 1
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
