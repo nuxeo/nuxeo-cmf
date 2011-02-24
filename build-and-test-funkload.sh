a906ca76185a262442b9835f1cd60097bf20ae77
Mariana Cedica <mcedica@nuxeo.com>
2011-02-24 16:23:14 +0100

    NXCM-290: add scrip to run CMF funkload tests under CI

---
+++ build-and-test-funkload.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+PROFILE=${1:-"server"}
+DISTRIBUTION=${2:-$PROFILE}
+DIR_DISTRIB=${3:-"cm"}
+
+echo "using distribution: $DISTRIBUTION"
+# Build Nuxeo Case Management
+mvn clean install || exit 1
+mvn -P$PROFILE -f nuxeo-case-management-distribution/pom.xml clean install -o || exit 1
+
+# start JBoss
+(cd nuxeo-case-management-distribution/target && unzip nuxeo-case-management-distribution-*.zip && rm *.zip) || exit 1
+chmod +x nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl || exit 1
+sed -i "s/-Xmx1024m/-Xmx2g/" nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeo.conf
+
+nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl start || exit 1
+
+# Run selenium tests with funkload profile
+HIDE_FF=true nuxeo-case-management-distribution/ftest/selenium/run.sh -funkload
+
+cd nuxeo-case-management-distribution/ftest/funkload 
+make createUsers
+make testCMF
+ret1=$?
+#Run funkload tests
+
+# Strop JBoss
+cd ../../..
+nuxeo-case-management-distribution/target/nuxeo-${DIR_DISTRIB}-$DISTRIBUTION/bin/nuxeoctl stop || exit 1
+
+# Exit if some tests failed
+[ $ret1 -eq 0 ] || exit 9
