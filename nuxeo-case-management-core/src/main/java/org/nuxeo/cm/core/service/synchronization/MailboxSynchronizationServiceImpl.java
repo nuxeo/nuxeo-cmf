ea88facedccb8d8dad1f79398837de1ba123bb7f
Sun Seng David TAN <stan@nuxeo.com>
2011-06-21 12:26:02 +0200

    NXCM-405 Fixing mailbox synchronization for mailbox name with a minus character

--- nuxeo-case-management-core/src/main/java/org/nuxeo/cm/core/service/synchronization/MailboxSynchronizationServiceImpl.java
+++ nuxeo-case-management-core/src/main/java/org/nuxeo/cm/core/service/synchronization/MailboxSynchronizationServiceImpl.java
@@ -545,7 +545,7 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '+' || c == '-' || c == '!' || c == '"' || c == '\'') {
+            if (c == '+' || c == '!' || c == '"' || c == '\'') {
                 sb.append('\\');
             }
             sb.append(c);
