25eb052885d2d0c130c9bbbd0b4c46fc5e847e5e
Laurent Doguin <ldoguin@nuxeo.com>
2011-08-02 19:12:09 +0200

    NXCM-440 update CMF release script

--- release.sh
+++ release.sh
@@ -11,7 +11,18 @@ sed -i s/$CURRENT_VERSION/$RELEASE_VERSION/g ./nuxeo-case-management-distributio
 sed -i s/$CURRENT_VERSION/$RELEASE_VERSION/g ./nuxeo-case-management-distribution/src/main/resources/cmf-funkload/nuxeo.defaults
 
 
-#Build and copy
-mvn -Prelease,enableWizard,tomcat,tomcatCorr,server,serverCorr install
+#Build and deploy the different packages
+#mvn clean deploy -Pqa,release,tomcat,tomcatCorr,server,serverCorr 
+
+#Package release
+#mvn -PenableWizard,tomcat install
+mkdir $WORKSPACE
+cp -r ./nuxeo-case-management-distribution/target/stage/nuxeo-cm-tomcat $WORKSPACE/nuxeo-cmf-$RELEASE_VERSION-tomcat
+
+cd $WORKSPACE/
+chmod +x nuxeo-cmf-$RELEASE_VERSION-tomcat/*.command nuxeo-cmf-$RELEASE_VERSION-tomcat/bin/*ctl nuxeo-cmf-$RELEASE_VERSION-tomcat/bin/.sh nuxeo-cmf-$RELEASE_VERSION-tomcat/bin/*.command
+
+zip -r nuxeo-cmf-$RELEASE_VERSION-tomcat.zip  nuxeo-cmf-$RELEASE_VERSION-tomcat
+md5sum nuxeo-cmf-$RELEASE_VERSION-tomcat.zip | tee nuxeo-cmf-$RELEASE_VERSION-tomcat.md5 
 
 
