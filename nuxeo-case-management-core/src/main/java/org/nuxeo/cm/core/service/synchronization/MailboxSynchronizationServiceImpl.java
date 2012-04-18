c21446b6bdce27ce6bf16b188ab0f3b91f9edf71
Sun Seng David TAN <stan@nuxeo.com>
2012-04-18 15:07:56 +0200

    Revert "NXP-8940: Fix mailbox synchro transaction management if transaction already started"
    This reverts commit 3b3a635c708b2b4d64c2fe219d04ec9c7540aa72.

--- nuxeo-case-management-core/src/main/java/org/nuxeo/cm/core/service/synchronization/MailboxSynchronizationServiceImpl.java
+++ nuxeo-case-management-core/src/main/java/org/nuxeo/cm/core/service/synchronization/MailboxSynchronizationServiceImpl.java
@@ -260,7 +260,7 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                         if (groupChilds != null && !groupChilds.isEmpty()) {
                             nextChildrenBatch.put(synchronizerId, groupChilds);
                         }
-                        if (++count % batchSize == 0 && count != 0) {
+                        if (++count % batchSize == 0) {
                             if (txStarted) {
                                 log.debug("Transaction ended during Mailbox synchronization");
                                 TransactionHelper.commitOrRollbackTransaction();
@@ -311,8 +311,7 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                 try {
                     userModel = userManager.getUserModel(userId);
                     if (userModel == null) {
-                        log.error("Could not synchronize mailbox for user "
-                                + userId);
+                        log.error("Could not synchronize mailbox for user " + userId);
                         continue;
                     }
                     synchronizerId = String.format("%s:%s", directoryName,
@@ -322,7 +321,7 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                             userManager.getUserSchemaName(), "",
                             synchronizerId, userId, generatedTitle, userId,
                             type, now, coreSession);
-                    if (++count % batchSize == 0 && count != 0) {
+                    if (++count % batchSize == 0) {
                         if (txStarted) {
                             log.debug("Transaction ended during Mailbox synchronization");
                             TransactionHelper.commitOrRollbackTransaction();
@@ -618,9 +617,8 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                     log.info("Start groups synchronization");
                     count = 0;
                     total = userManager.getGroupIds().size();
-                    Boolean txStarted = true;
+                    boolean txStarted = false;
                     try {
-                        // Check if transaction started
                         if (!TransactionHelper.isTransactionActive()) {
                             txStarted = TransactionHelper.startTransaction();
                             log.debug("New Transaction started during Mailbox synchronization");
@@ -638,8 +636,6 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                         if (txStarted) {
                             TransactionHelper.commitOrRollbackTransaction();
                             log.debug("Transaction ended during Mailbox synchronization");
-                            TransactionHelper.startTransaction();
-                            log.debug("New Transaction started during Mailbox synchronization");
                         }
                     }
                     log.info("Looking for deleted group entries");
@@ -664,9 +660,9 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                     log.debug("Start users synchronization");
                     count = 0;
                     total = userIds.size();
-                    Boolean txStarted = true;
+
+                    boolean txStarted = false;
                     try {
-                        // Check if transaction started
                         if (!TransactionHelper.isTransactionActive()) {
                             txStarted = TransactionHelper.startTransaction();
                             log.debug("New Transaction started during Mailbox synchronization");
@@ -684,8 +680,6 @@ public class MailboxSynchronizationServiceImpl extends DefaultComponent
                         if (txStarted) {
                             TransactionHelper.commitOrRollbackTransaction();
                             log.debug("Transaction ended during Mailbox synchronization");
-                            TransactionHelper.startTransaction();
-                            log.debug("New Transaction started during Mailbox synchronization");
                         }
                     }
                     log.debug(String.format("Updated %d/%d mailboxes", count,
